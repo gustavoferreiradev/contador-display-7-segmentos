02a3f947cfdbee7ccd8c4491727dc8eaa48d18ef
Gustavo Ferreira da Silva <gustavofersilva@mail.com>
2018-03-27 13:49:05 -0300

    Adicionado o readme bem como as duas instâncias do projeto

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# contador-display-7-segmentos
-Projeto desenvolvido para um display de 7 segmentos utilizando-se do Shield Arduino Garoa Dojo.
+# Contador para display de 7 segmentos
+Projeto desenvolvido para um display de 7 segmentos utilizando-se do Shield [Arduino Garoa Dojo](https://garoa.net.br/wiki/Garoa_Dojo_Shield).
---
+++ contador-crescente-decrescente/contador-crescente-e-decrescente.ino
@@ -0,0 +1,167 @@
+int ledA = 7;
+int ledB = 8;
+int ledC = 9;
+int ledD = 10;
+int ledE = 11;
+int ledF = 12;
+int ledG = 13;
+
+
+void setup(){
+
+pinMode(ledA, OUTPUT);
+pinMode(ledB, OUTPUT);
+pinMode(ledC, OUTPUT);
+pinMode(ledD, OUTPUT);
+pinMode(ledE, OUTPUT);
+pinMode(ledF, OUTPUT);
+pinMode(ledG, OUTPUT);
+
+
+}
+void loop(){
+crescente();
+decrescente();
+
+}  
+
+void numero0(){
+digitalWrite(ledA, LOW);
+digitalWrite(ledB, HIGH);
+digitalWrite(ledC, HIGH);
+digitalWrite(ledD,HIGH);
+digitalWrite(ledE, HIGH);
+digitalWrite(ledF, HIGH);
+digitalWrite(ledG, HIGH);
+delay(1000);
+
+
+}
+void numero1(){
+digitalWrite(ledA, LOW);
+digitalWrite(ledB, LOW);
+digitalWrite(ledC, HIGH);
+digitalWrite(ledD,HIGH);
+digitalWrite(ledE, LOW);
+digitalWrite(ledF, LOW);
+digitalWrite(ledG,LOW);
+delay(1000);
+
+
+}
+
+
+void numero2(){
+digitalWrite(ledA, HIGH);
+digitalWrite(ledB, HIGH);
+digitalWrite(ledC, HIGH);
+digitalWrite(ledD,LOW);
+digitalWrite(ledE, HIGH);
+digitalWrite(ledF, HIGH);
+digitalWrite(ledG,LOW);
+delay(1000);
+}
+
+void numero3(){
+digitalWrite(ledA, HIGH);
+digitalWrite(ledB, HIGH);
+digitalWrite(ledC, HIGH);
+digitalWrite(ledD,HIGH);
+digitalWrite(ledE, HIGH);
+digitalWrite(ledF, LOW);
+digitalWrite(ledG,LOW);
+delay(1000);
+}
+
+void numero4(){
+digitalWrite(ledA, HIGH);
+digitalWrite(ledB,LOW);
+digitalWrite(ledC, HIGH);
+digitalWrite(ledD,HIGH);
+digitalWrite(ledE, LOW);
+digitalWrite(ledF, LOW);
+digitalWrite(ledG,HIGH);
+delay(1000);
+}
+void numero5(){
+digitalWrite(ledA, HIGH);
+digitalWrite(ledB, HIGH);
+digitalWrite(ledC, LOW);
+digitalWrite(ledD,HIGH);
+digitalWrite(ledE, HIGH);
+digitalWrite(ledF, LOW);
+digitalWrite(ledG,HIGH);
+delay(1000);
+}
+
+void numero6(){
+digitalWrite(ledA, HIGH);
+digitalWrite(ledB, HIGH);
+digitalWrite(ledC, LOW);
+digitalWrite(ledD,HIGH);
+digitalWrite(ledE, HIGH);
+digitalWrite(ledF, HIGH);
+digitalWrite(ledG,HIGH);
+delay(1000);
+
+}
+
+void numero7(){
+digitalWrite(ledA, LOW);
+digitalWrite(ledB, HIGH);
+digitalWrite(ledC, HIGH);
+digitalWrite(ledD,HIGH);
+digitalWrite(ledE, LOW);
+digitalWrite(ledF, LOW);
+digitalWrite(ledG, LOW);
+delay(1000);
+}
+
+void numero8(){
+digitalWrite(ledA, HIGH);
+digitalWrite(ledB, HIGH);
+digitalWrite(ledC, HIGH);
+digitalWrite(ledD,HIGH);
+digitalWrite(ledE, HIGH);
+digitalWrite(ledF, HIGH);
+digitalWrite(ledG, HIGH);
+delay(1000);
+}
+
+void numero9(){
+digitalWrite(ledA, HIGH);
+digitalWrite(ledB, HIGH);
+digitalWrite(ledC, HIGH);
+digitalWrite(ledD,HIGH);
+digitalWrite(ledE, HIGH);
+digitalWrite(ledF, LOW);
+digitalWrite(ledG, HIGH);
+delay(1000);
+}
+
+void crescente(){
+numero0();
+numero1();
+numero2();
+numero3();
+numero4();
+numero5();
+numero6();
+numero7();
+numero8();
+numero9();
+}
+
+void decrescente(){
+numero8();
+numero7();
+numero6();
+numero5();
+numero4();
+numero3();
+numero2();
+numero1();
+}
+
+
+
